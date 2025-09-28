5f5c969df915659b8675e63d0d5e045fb954200d
Menmery <2053186460@qq.com>
2025-09-28 15:58:32 +0800

    1

---
+++ 31_添加泛型约束.ts
@@ -0,0 +1,16 @@
+interface ILength {
+  length: number
+}
+
+// 传入的类型必须要有length属性
+function id<Type extends ILength>(value: Type): Type {
+  value.length
+  return value
+}
+
+id(['a', 'c'])
+id('abc')
+id({ length: 10, name: 'jack' })
+
+// 错误演示
+// id(123)
